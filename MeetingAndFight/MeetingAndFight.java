978ee94a70bcc9eed437f153d95d52887fd73199
bart3br <95648151+bart3br@users.noreply.github.com>
2021-12-08 17:50:11 +0100

    dodana statyczna metoda Fight

--- MeetingAndFight/MeetingAndFight.java
+++ MeetingAndFight/MeetingAndFight.java
@@ -1,9 +1,14 @@
 package game.TeamProjectGame.MeetingAndFight;
 
 import game.TeamProjectGame.Characters.*;
+import game.TeamProjectGame.Characters.Character;
+import game.TeamProjectGame.Characters.Friends.Friend;
+import game.TeamProjectGame.Characters.Villains.Dragon;
+import game.TeamProjectGame.Characters.Villains.Villain;
+
+import javax.xml.stream.events.Characters;
 
 class MeetingAndFight{
-
 
     public static void Meeting(Character char1, Character char2) {
         // Przyjaciel i przyjaciel, tu nie ma walki to dokończe później
@@ -22,5 +27,29 @@ class MeetingAndFight{
         }
     }
 
+    public static void Fight (Character friend, Character enemy)
+    {
 
+        if (enemy instanceof Dragon){
+            friend.setHp(0);
+            System.out.println("You lost the fight!");
+        }
+        else{
+            if (friend.getSpeed()>enemy.getSpeed())
+                enemy.setHp(enemy.getHp()-friend.getDmg());
+
+            while (friend.getHp()>0 && enemy.getHp>0)
+            {
+                friend.setHp(friend.getHp()-enemy.getDmg());
+                enemy.setHp(enemy.getHp()-friend.getDmg());
+            }
+            /*jezeli friend zginie w trakcie przejscia petli
+            to nie ma znaczenia ze zada cios po smierci bo i tak game over*/
+
+            if (friend.getHp()>0)
+                System.out.println("You won the fight!");
+            else
+                System.out.println("You lost the fight!");
+        }
+    }
 }
