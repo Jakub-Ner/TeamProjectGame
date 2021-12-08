cae4113a1b1318269d09e7e21f6926a60a1ffcd4
Adrian Broniecki <trine20a@gmail.com>
2021-12-08 18:30:06 +0100

    Uzupełnienie przyjaciół.

--- Characters/Friends/Dwarf.java
+++ Characters/Friends/Dwarf.java
@@ -1,6 +1,20 @@
 package game.TeamProjectGame.Characters.Friends;
 
 import game.TeamProjectGame.Characters.Character;
+import game.TeamProjectGame.Cries.src.*;
 
 public class Dwarf extends Friend {
+    public Dwarf(){
+        super(100,12,3,"Murdoch");
+    }
+
+    public void getBattleDwarvenCry(){
+        setCry(new BattleDwarvenCry());
+        this.cry.shout();
+    }
+
+    public void getFriendlyDwarvenCry(){
+        setCry(new FriendlyDwarvenCry());
+        this.cry.shout();
+    }
 }
--- Characters/Friends/Friend.java
+++ Characters/Friends/Friend.java
@@ -1,7 +1,40 @@
 package game.TeamProjectGame.Characters.Friends;
 
 import game.TeamProjectGame.Characters.Character;
+import game.TeamProjectGame.Cries.src.Cry;
 
 public abstract class Friend extends Character {
 
+    protected Cry cry;
+    private String name;
+
+    public Friend(){
+        super();
+    }
+
+    public Friend(int hp, int dmg, int speed,String name){
+        super(hp,dmg,speed);
+    }
+
+    public Cry getCry() {
+        return cry;
+    }
+
+    public void setCry(Cry cry) {
+        this.cry = cry;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Your stats:\n" + "Health points:" + this.getHp() + "\n\t" +
+        "Damage: " + this.getDmg() + "\n\t" + "Speed: " + this.getSpeed() + "\n";
+    }
 }
