467ec7b24c3369c6f3b6c277e4e887aacb1600f6
Houdini888 <56999380+Houdini888@users.noreply.github.com>
2021-12-08 20:24:45 +0100

    Villains added

--- Characters/Villains/Orc.java
+++ Characters/Villains/Orc.java
@@ -3,4 +3,9 @@ package game.TeamProjectGame.Characters.Villains;
 import game.TeamProjectGame.Characters.Character;
 
 public class Orc extends Villain {
+
+    public Orc() {
+        super(50,6,3);
+    }
+
 }
--- Characters/Villains/Phantom.java
+++ Characters/Villains/Phantom.java
@@ -3,4 +3,8 @@ package game.TeamProjectGame.Characters.Villains;
 import game.TeamProjectGame.Characters.Character;
 
 public class Phantom extends Villain {
+
+    public Phantom() {
+        super(25,13,5);
+    }
 }
