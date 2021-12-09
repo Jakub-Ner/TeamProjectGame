17c10430908169bf79703f1fed73115ffa78835f
JingleBellczyk <agabela121@gmail.com>
2021-12-09 19:56:25 +0100

    act2

--- Menu.java
+++ Menu.java
@@ -3,14 +3,38 @@ package game.TeamProjectGame;
 import game.TeamProjectGame.Characters.Character;
 import game.TeamProjectGame.Characters.CharacterFactory;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
-        public static void Start(){
+
+
+        public static void start(){
                 System.out.println("Welcome to our game!");
         }
-        public static Character chooseCharacterMenu(){
+        public static int choosingNumber(int options){ //sprawdza czy uzytkownik podaje dobrą liczbe
+
                 Scanner scanner = new Scanner(System.in);
+
+                int input;
+
+                try{input = scanner.nextInt();} catch (InputMismatchException e){
+                        scanner.nextLine();
+                        input = -1;
+                }
+
+                while(0 > input || options <= input){
+                        System.out.print("Answer out of reach! Try again.\n>");
+
+                        try{input = scanner.nextInt();} catch (InputMismatchException e){
+                                scanner.nextLine();
+                                input = -1;
+                        }
+                }
+
+                return input;
+        }
+        public static Character chooseCharacterMenu(){ //obowiązkowo do wywołania na początku
                 System.out.println("Choose number of a character:\n" +
                         "1 - HUMAN\n" +
                         "2 - DWARF\n" +
@@ -19,9 +43,10 @@ public class Menu {
                         "5 - PHANTOM\n" +
                         "6 - DRAGON\n");
 
-                int n = scanner.nextInt() - 1;
-                return CharacterFactory.addCharacter(n);
+                int n = choosingNumber(7);
+                return CharacterFactory.addCharacter(n-1);
         }
+
         public static void printCharacters(){
                 for(int i = 0; i< CharacterFactory.getCharacters().size(); i++)
                 {
@@ -31,7 +56,18 @@ public class Menu {
                         + "Speed"     + CharacterFactory.getCharacters().get(i).getSpeed());
                 }
         }
-        //public static void
+        public static int chooseAction(){
+                System.out.println("What do you want to do? Choose number:\n" +
+                        "1 - print a list of Characters\n" +
+                        "2 - end the game\n");
+                int n = choosingNumber(3);
+
+                if(n==1) printCharacters();
+                if(n==2) end();
+        }
+        public static void end(){
+                System.out.println("Goodbye!");
+        }
 
 
 }
