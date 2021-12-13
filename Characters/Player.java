ae26b3cc85f4a5304a1e2254ef9bf5f0312b3bde
qmatix <95592306+qmatix@users.noreply.github.com>
2021-12-13 11:03:09 +0100

    Update Player.java

--- Characters/Player.java
+++ Characters/Player.java
@@ -2,7 +2,7 @@ package game.TeamProjectGame.Characters;
 
 import game.TeamProjectGame.Characters.Friends.Friend;
 
-public class Player extends Character {
+public class Player extends Character implements Serializable{
 	/**
 	 * Initializes players with stats of said Friend.
 	 * It MUST NOT use NPCFactory to initialize itself
