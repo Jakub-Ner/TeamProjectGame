8af633847b32726b8a6e88fe1bcba1149ea62ae1
Houdini888 <56999380+Houdini888@users.noreply.github.com>
2021-12-13 21:37:07 +0100

    added NpcAPI

---
+++ API/NpcAPI.java
@@ -0,0 +1,43 @@
+package game.TeamProjectGame.API;
+
+import game.TeamProjectGame.Characters.NPCFactory;
+import game.TeamProjectGame.Characters.Npc;
+
+import java.io.*;
+
+public class NpcAPI implements Serializable {
+
+    public void SaveNPC() {
+
+        try {
+            ObjectOutputStream so = new ObjectOutputStream(new FileOutputStream("PlikNPC"));
+
+            for(int i = 0; i < NPCFactory.getCharacters().size(); i++) {
+
+                so.writeObject(NPCFactory.getCharacters().get(i));
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void LoadNPC() {
+
+        try{
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("PlikNPC"));
+            Object czyBrakObiektu = null;
+
+            while ( (czyBrakObiektu = is.readObject()) != null ) {
+
+                Object addedNPC = is.readObject();
+
+                NPCFactory.addCharacter( (Npc) addedNPC );
+            }
+
+        }catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
