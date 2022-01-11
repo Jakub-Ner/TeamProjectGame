dc6ab556d7cafc9ee57b195ac3a386f3c9a280dd
JarekZagorski <me.kubolski@gmail.com>
2022-01-11 19:25:25 +0100

    Poprawy pakietyzacji

--- Main/MenuGUI.java
+++ Main/MenuGUI.java
@@ -1,65 +1,155 @@
-package game.TeamProjectGame;
+package game.TeamProjectGame.Main;
 
-import game.TeamProjectGame.API.*;
-import game.TeamProjectGame.Board.*;
-import game.TeamProjectGame.Characters.Friends.*;
-import game.TeamProjectGame.Characters.Player;
+import game.TeamProjectGame.Characters.Friends.Dwarf;
+import game.TeamProjectGame.Characters.Friends.Elf;
+import game.TeamProjectGame.Characters.Friends.Friend;
+import game.TeamProjectGame.Characters.Friends.Human;
 import game.TeamProjectGame.MovePattern.SquarePattern;
-import game.TeamProjectGame.Game;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.Serializable;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
-import static game.TeamProjectGame.API.BoardAPI.LoadBoard;
+public class MenuGUI {
 
-public class Menu implements Serializable {
+    private int framesize_X = 450;
+    private int framesize_Y = 200;
 
-    MenuGUI menuGUI = new MenuGUI();
+    private JDialog frame = new JDialog();
 
-    public static void printStats(Player player) {
-        System.out.println(player.getClass().getSimpleName()
-                + "\tHP: " + player.getHp()
-                + "\tDmg: " + player.getDmg()
-                + "\tSpeed: " + player.getSpeed());
+    private JLabel welcome = new JLabel("Welcome to our game!");
+
+    private JRadioButton loadButton1 = new JRadioButton("New game");
+    private JRadioButton loadButton2 = new JRadioButton("Load game from file");
+    private ButtonGroup loadGroup = new ButtonGroup();
+
+    private JLabel charLabel = new JLabel("Choose your character:");
+    private JRadioButton charButton1 = new JRadioButton("Human");
+    private JRadioButton charButton2 = new JRadioButton("Elf");
+    private JRadioButton charButton3 = new JRadioButton("Dwarf");
+    private ButtonGroup charGroup = new ButtonGroup();
+
+    private JButton startButton = new JButton("Play!");
+
+    private JPanel panel1 = new JPanel();
+    private JPanel panel2 = new JPanel();
+    private JPanel panel3 = new JPanel();
+    private JPanel panel4 = new JPanel();
+    private JPanel panel5 = new JPanel();
+
+
+    private boolean startNewGame;
+
+    private Friend chosenCharacter;
+
+    public boolean isStartNewGame() {
+        return startNewGame;
     }
 
-    public void load() {
-        File file = new File("Player.ser");
+    public Friend getChosenCharacter() {
+        return chosenCharacter;
+    }
 
-        if (PlayerAPI.checkFile(file)) {
+    //konstruktor z default wyborami.
+    public MenuGUI() {
+        this.startNewGame = true;
+        this.chosenCharacter = new Human(new SquarePattern());
+    }
 
-            if (!menuGUI.isStartNewGame()) {
-                Game.player = PlayerAPI.loadPlayer();
-                Game.board = new Board(Game.player);
-                LoadBoard(Game.board);
-                NpcAPI.LoadNPC();
-            } else {
-                startGame();
+    public void drawMenu() {
+
+        frame.getContentPane().add(BorderLayout.NORTH, panel1);
+        frame.getContentPane().add(BorderLayout.WEST, panel2);
+        frame.getContentPane().add(BorderLayout.CENTER, panel3);
+        frame.getContentPane().add(BorderLayout.EAST, panel4);
+
+        panel1.add(welcome);
+
+        loadGroup.add(loadButton1);
+        loadGroup.add(loadButton2);
+
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+        panel2.add(loadButton1);
+        panel2.add(loadButton2);
+
+        loadButton1.addActionListener(new loadButtonReaction());
+        loadButton2.addActionListener(new loadButtonReaction());
+
+        charGroup.add(charButton1);
+        charGroup.add(charButton2);
+        charGroup.add(charButton3);
+
+        panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
+        panel3.add(charLabel);
+        panel3.add(charButton1);
+        panel3.add(charButton2);
+        panel3.add(charButton3);
+
+        charButton1.addActionListener(new charButtonReaction());
+        charButton2.addActionListener(new charButtonReaction());
+        charButton3.addActionListener(new charButtonReaction());
+
+        panel4.add(startButton);
+
+        startButton.addActionListener(new startButtonReaction());
+
+
+
+        loadButton1.setSelected(true);
+        charButton1.setSelected(true);
+
+        frame.setModal(true);
+
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        frame.setSize(framesize_X,framesize_Y);
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+    }
+
+    public class loadButtonReaction implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if (e.getSource() == loadButton1) {
+
+                startNewGame = true;
             }
-        } else {
-            startGame();
+            if(e.getSource() == loadButton2) {
+
+                startNewGame = false;
+            }
         }
     }
 
-    public void startGame() { //from the beginning
-        Game.player = new Player(menuGUI.getChosenCharacter());
-        Game.board = new Board(Game.player);
-        NPC_generator_NEW.generateNPC(Game.board);
+    public class charButtonReaction implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if(e.getSource() == charButton1) {
+
+                chosenCharacter = new Human(new SquarePattern());
+            }else if(e.getSource() == charButton2) {
+
+                chosenCharacter = new Dwarf(new SquarePattern());
+            }else if(e.getSource() == charButton3) {
+
+                chosenCharacter = new Elf(new SquarePattern());
+            }
+        }
     }
 
-    public void startMenu() {
+    public class startButtonReaction implements ActionListener {
 
-        menuGUI.drawMenu();
-        load();
-
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            frame.dispose();
+        }
     }
-
-
 
 }
