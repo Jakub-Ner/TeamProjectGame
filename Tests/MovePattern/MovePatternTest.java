220e2b013a235e0f59435bec542de796ca311119
Jakub-Ner <kubaner1@gmail.com>
2022-01-20 21:17:15 +0100

    add jupiter-params

---
+++ Tests/MovePattern/MovePatternTest.java
@@ -0,0 +1,12 @@
+package MovePattern;
+
+import Characters.Character;
+import Characters.Friends.Human;
+import Characters.Player;
+import Main.Game;
+import MovePattern.SquarePattern;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MovePatternTest {
+}
